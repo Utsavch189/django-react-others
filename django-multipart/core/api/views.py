2c10d851e2b5cf282bb09ccb04233d91eedc68bd
Utsavch189 <utsavpokemon9000chatterjee@gmail.com>
2024-01-17 12:23:34 +0530

    upload file by formdata in js to rest api

--- django-multipart/core/api/views.py
+++ django-multipart/core/api/views.py
@@ -3,6 +3,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from .serializers import PeopleSerializer
 from .models import People
+import base64
+from django.conf import settings
 
 class ApiController(APIView):
 
@@ -13,16 +15,27 @@ class ApiController(APIView):
             return Response({"message":"fetched!","users":data},status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"message":e},status=status.HTTP_400_BAD_REQUEST)
+        
     
     def post(self,request):
         try:
             serializer=PeopleSerializer(data=request.data)
+            print(request.data) # <QueryDict: {'name': ['Sanu'], 'username': ['san87'], 'pic': [<InMemoryUploadedFile: bikess.jpg (image/jpeg)>]}>
             file=request.FILES['pic']
-            print(file.chunks)
+            """
+            print(type(file))  # <class 'django.core.files.uploadedfile.InMemoryUploadedFile'>
+            print(type(file.read()))  # <class 'bytes'>
+            print(type(str(file.read())))  # <class 'str'>
+
+            """
+            base_64_encode=base64.b64encode(file.read())
+            with open('encode.txt','wb') as f:
+                f.write(base_64_encode)
             if serializer.is_valid():
                 serializer.save()
                 return Response({"message":"created!","user":serializer.data},status=status.HTTP_201_CREATED)
             else:
                 return Response({"message":serializer.errors},status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return Response({"message":e},status=status.HTTP_400_BAD_REQUEST)
+            print(str(e))
+            return Response({"message":str(e)},status=status.HTTP_400_BAD_REQUEST)
--- django-multipart/core/core/settings.py
+++ django-multipart/core/core/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+CORS_ORIGIN_ALLOW_ALL=True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
